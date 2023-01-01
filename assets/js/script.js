d8f873808f3ca0ba4dd70e6566493dddb0920471
DTekeste <tekeste.david@yahoo.com>
2022-12-31 20:32:00 -0800

    added JS to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var baseUrl = "https://restcountries.com/v3.1/name/";
+var baseUrl2 = "https://www.travel-advisory.info/api?countrycode="
 
 
 
@@ -6,8 +7,11 @@ var baseUrl = "https://restcountries.com/v3.1/name/";
 function search(){
     var formName = document.getElementById("user-form");
     var countryName = formName.elements[0].value;
-    //var output = "";
     var Languages = "";
+    //var Currency = [];
+    var currencyString = "";
+    var countryCode = "";
+    
    
     console.log(countryName)
     var requestUrl = baseUrl + countryName;
@@ -15,28 +19,36 @@ function search(){
        return response.json();     
     }).then(function(data) 
     {
-        //output = data
         console.log(data);
-        console.log("<img src=\""+String(data[0]['flags']['png'])+"\" alt=\"Country Flag\"> ");
-
+        //console.log("<img src=\""+String(data[0]['flags']['png'])+"\" alt=\"Country Flag\"> ");
+        countryCode = data[0]['cca2'];
 
        // returning language information 
         for ( index in data[0]['languages']) {
-            //console.log(data[0]['languages'][index]);
-            Languages += data[0]['languages'][index] + " <br>";
+            Languages += data[0]['languages'][index] + " <br>";  
         }
+        for ( index in data[0]['currencies']) {
+            //Currency.push([data[0]['currencies'][index]['name'], data[0]['currencies'][index]['symbol']])
+            currencyString += data[0]['currencies'][index]['name'] + "  "+ data[0]['currencies'][index]['symbol'] + " <br>";
+            //console.log(currencyString);  
+        }
+        
         document.getElementById("Flag").innerHTML="<img src=\""+String(data[0]['flags']['png'])+"\" alt=\"Country Flag\">"
         document.getElementById("Languages").innerHTML=Languages;
-        document.getElementById("population").innerHTML=data[0]['population'];
+        document.getElementById("population").innerHTML=data[0]['population'].toLocaleString("en-US");
+        document.getElementById("capital").innerHTML=data[0]['capital'];
+        document.getElementById("currencies").innerHTML=currencyString;
+        var requestUrl2 = baseUrl2 + countryCode;
+        console.log(requestUrl2);
+        fetch(requestUrl2).then(function(response) {
+            return response.json();     
+        }).then(function(data) 
+        {
+            console.log(data);
+            console.log(data['data'][countryCode]['advisory']);
+            document.getElementById("message").innerHTML=data['data'][countryCode]['advisory']['message'];
+            document.getElementById("source").innerHTML="<a href=\"" + data['data'][countryCode]['advisory']['source']+ "\">"+data['data'][countryCode]['advisory']['source']+"</a>";
+        });
     });
-    
+     
 }
-
-// plain JavaScript
-
-
-
-// jQuery method
-//$.ajax(requestUrl).then(function(response) {
-   // console.log(response);
-//});
